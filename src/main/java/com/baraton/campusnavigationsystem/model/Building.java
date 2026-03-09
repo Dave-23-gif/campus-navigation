816580f23064364d4c88c2a0fa4ab41387de5bc3
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-09 08:11:45 +0300

    Created Building model

---
+++ src/main/java/com/baraton/campusnavigationsystem/model/Building.java
@@ -0,0 +1,21 @@
+package com.baraton.campusnavigationsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "buildings")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Building {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private double latitude;
+    private double longitude;
+    private String description;
+}
