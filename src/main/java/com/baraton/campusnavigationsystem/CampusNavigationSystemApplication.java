bb63ddc64637ece40b47b5ec4066a1ccc287a1ea
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-09 08:04:37 +0300

    first commit

---
+++ src/main/java/com/baraton/campusnavigationsystem/CampusNavigationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.baraton.campusnavigationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNavigationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusNavigationSystemApplication.class, args);
+    }
+
+}
