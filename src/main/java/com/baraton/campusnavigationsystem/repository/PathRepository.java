623a6e1ffe9f2621ad2417bd25c4911f515ab76d
Dave-23-gif <chikamaidavid98@gmail.com>
2026-03-19 18:37:39 +0300

    Created PathRepository

---
+++ src/main/java/com/baraton/campusnavigationsystem/repository/PathRepository.java
@@ -0,0 +1,7 @@
+package com.baraton.campusnavigationsystem.repository;
+
+import com.baraton.campusnavigationsystem.model.Path;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PathRepository extends JpaRepository<Path, Long> {
+}
